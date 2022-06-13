6666117d3aa9e3adcfe1ee28bc986efb33d072b2
alyAzazy <105085209+alyAzazy@users.noreply.github.com>
2022-06-13 10:23:40 +0200

    last changes

--- api/index.js
+++ api/index.js
@@ -9,7 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/api/shipments/:order_id', async (req, res) => {
   try{
